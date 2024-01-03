cba110f633d7a813b99352e9b6e078d993399c63
Alex Spies <97462738+AlexanderSpies@users.noreply.github.com>
2024-01-03 14:41:28 -0500

    Add files via upload

--- src/index.js
+++ src/index.js
@@ -1,18 +1,30 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
-import Header from './pages/Components/header';
-import Home from './pages/home/home'
-import reportWebVitals from './reportWebVitals';
+import ReactDOM from 'react-dom';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+
+import Header from '../src/pages/Components/header.jsx';
+import Home from '../src/pages/home/home';
+import Recipes from '../src/pages/Recipes/Recipes';
+import Drinks from '../src/pages/Drinks/Drinks';
+import Upload from '../src/pages/Upload/Upload';
+import About from '../src/pages/About/About';
+
+const App = () => {
+  return (
+    <Router>
+      <Header/>
+      <React.StrictMode>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/recipes" element={<Recipes />} />
+          <Route path="/drinks" element={<Drinks />} />
+          <Route path="/upload" element={<Upload />} />
+          <Route path="/about" element={<About />} />
+        </Routes>
+      </React.StrictMode>
+    </Router>
+  );
+};
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <Header/>
-    <Home />
-  </React.StrictMode>
-);
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+root.render(<App />);
