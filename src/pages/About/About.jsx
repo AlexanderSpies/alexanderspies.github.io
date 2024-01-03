094f212f5e50da441c92722f038356ffe3a6e985
Alex Spies <97462738+AlexanderSpies@users.noreply.github.com>
2024-01-03 14:36:18 -0500

    Add files via upload

---
+++ src/pages/About/About.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import '../../css/index.css';
+
+const Home = () => {
+    return (
+        <div className="home-container">
+            <div className="info-block">
+                <h1 className='sectionHeading'>About Page</h1>
+                <p className='Info'>here is the about page</p>
+            </div>
+        </div>
+    );
+}
+
+export default Home;
