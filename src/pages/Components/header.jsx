6d8cceabccca85f81ac27df99819161fda0d850b
Alex Spies <spiesa2@winthrop.edu>
2024-01-03 21:45:21 -0500

    Merge branch 'staging' of github.com:AlexanderSpies/SpiesRecipeBook

--- src/pages/Components/header.jsx
+++ src/pages/Components/header.jsx
@@ -1,25 +1,19 @@
 import React, { useState } from 'react';
-import '../../Css/index.css';
+import ReactDOM from 'react-dom';
+import '../../css/index.css';
 
 const Header = () => {
-  const [menuExpanded, setMenuExpanded] = useState(false);
-
-  const toggleMenu = () => {
-    setMenuExpanded(!menuExpanded);
-  };
-
   return (
     <div className='container'>
         <div className='titleContainer'>
-          <h1 className='title'>Generations Of Taste</h1>
-        </div>
+          <a className='title' href='/'>TASTE</a>
         <div className='menuContainer'>
           <div className='menuOptions'>
-            <a className='menuOption' href="/">Home </a>
-            <a className='menuOption' href="/about">About </a>
-            <a className='menuOption' href="/recipes">Recipes </a>
-            <a className='menuOption' href="/sauces">Drinks </a>
+            <a className='menuOption' href="/recipes">Recipes</a>
+            <a className='menuOption' href="/drinks">Drinks</a>
+            <a className='menuOption' href="/upload">Upload</a>
           </div>
+        </div>
         </div>
     </div>
   );
--- src/pages/home/home.jsx
+++ src/pages/home/home.jsx
@@ -1,23 +1,42 @@
 import React from 'react';
-import '../../Css/index.css';
+import '../../css/index.css';
+import triangleBackground from '../../imgs/5169614.png'
 
 const Home = () => {
-    return (
-        <div className="home-container">
-            <div className="info-block">
-                <h1 className='sectionHeading'>Kitchen Wisdom</h1>
-                <p className='Info'>Embark on a heartwarming journey through generations of cherished recipes, where classic flavors and time-honored culinary traditions come together in perfect harmony. Our carefully curated collection brings a fresh and family-friendly twist to these beloved dishes, ensuring that everyone, from the littlest chef to the seasoned cook, finds joy in every bite.</p>
-            </div>
-            <div className="info-block">
-                <h1 className='sectionHeading'>Shake Up Your Social Life</h1>
-                <p className='Info'>Elevate your beverage game with a symphony of innovative cocktails that will awaken your senses and ignite your curiosity. Our drink recipes are more than just liquid refreshment; they are a fusion of unique flavors, tantalizing infusions, and well-guarded mixology secrets that promise to unlock a world of excitement.</p>
-            </div>
-            <div className="info-block">
-                <h1 className='sectionHeading'>Stirring Up the Online Pot</h1>
-                <p className='Info'>Share the magic of your favorite recipes with us. Your culinary creations have the power to bring smiles, satisfaction, and togetherness. Let's make our community richer in flavors and experiences. Join in, inspire others, and become a part of our flavorful family. Start sharing your cherished recipe today, and let the joy of cooking connect us all!</p>
-            </div>
+  return (
+    <div className="home-container">
+      <div className='Recipes'>
+        <div className='sectionHeading'>
+          <a href='/recipes'>
+            <h1>Unleash the Flavours of Homely Recipes</h1>
+            <p className='Info'>Introducing vintage, heart-touching kitchen specials that have been passed down for generations.</p>
+          </a>
         </div>
-    );
+      </div>
+      <div className="Drinks">
+        <div className='sectionHeading'>
+          <a href='/drinks'>
+              <h1>For Drink Connoisseurs and Amateurs Alike!</h1>
+              <p className='Info'>Savor intoxicating beverages, from local gems to international favorites that’ll tingle your taste buds.</p>
+          </a>
+          </div>
+      </div>
+      <div className="triangleContainer">
+        <img className='triangle' src={triangleBackground} alt="Triangle Background" />
+        <div className="overlay">
+          <h2>DECADES OF DELICIOUSNESS</h2>
+        </div>
+      </div>
+      <div className="Upload">
+        <div className='sectionHeading'>
+          <a  href='/upload'>
+              <h1>Our Melting Pot</h1>
+              <p className='Info'>Share the magic of your favorite recipes with us. Your culinary creations have the power to bring smiles, satisfaction, and togetherness. Let's make our community richer in flavors and experiences. Join in, inspire others, and become a part of our flavorful family. Start sharing your cherished recipe today, and let the joy of cooking connect us all!</p>
+          </a>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Home;
