607740ed045f0d9b38a641f749bb15ff7d0e578a
Alex Spies <spiesa2@winthrop.edu>
2024-01-04 22:07:39 -0500

    fixed some formatting

--- src/pages/home/home.jsx
+++ src/pages/home/home.jsx
@@ -9,38 +9,38 @@ import oldFashioned from '../../imgs/oldFashioned.png';
 
 const Home = () => {
   return (
-  <div className="home-container">
-    <div className='Recipes'>
-      <div className='sectionHeading'>
-        <a href='/recipes'>
-          <h1>Unleash the Flavours of Homely Recipes</h1>
-          <p className='Info'>Introducing vintage, heart-touching kitchen specials that have been passed down for generations.</p>
-        </a>
-      </div>
-      <div className='photoContainer'>
-        <img className='PastaPic' src={pasta} alt='pasta on form in front of dish'/>
-      </div>
-    </div>
-    <div className="Drinks">
-      <div className='sectionHeading'>
-        <a href='/drinks' className="contentContainer">
-          <img className='liquorPic' src={oldFashioned} alt='pasta on form in front of dish'/>
-          <div className="textContainer">
-            <h1>For Drink Connoisseurs and Amateurs Alike!</h1>
-            <p className='Info'>Savor intoxicating beverages, from local gems to international favorites that’ll tingle your taste buds.</p>
+    <div className="home-container">
+      <section className='Recipes'>
+        <a href='/recipes' className="contentContainer">
+          <div className='sectionHeading'>
+            <h1>Unleash the Flavours of Homely Recipes</h1>
+            <p className='Info'>Introducing vintage, heart-touching kitchen specials that have been passed down for generations.</p>
+          </div>
+          <div className='photoContainer'>
+            <img className='PastaPic' src={pasta} alt='Pasta in front of dish' />
           </div>
         </a>
+      </section>
+      <section className="Drinks">
+        <div className='sectionHeading'>
+          <a href='/drinks' className="contentContainer">
+            <img className='liquorPic' src={oldFashioned} alt='Old Fashioned drink' />
+            <div className="textContainer">
+              <h1>For Drink Connoisseurs and Amateurs Alike!</h1>
+              <p className='Info'>Savor intoxicating beverages, from local gems to international favorites that’ll tingle your taste buds.</p>
+            </div>
+          </a>
+        </div>
+      </section>
+      <div className="triangleContainer">
+        <img className='triangle' src={triangleBackground} alt="" aria-hidden="true" />
+        <div className="overlay">
+          <h2>DECADES OF DELICIOUSNESS</h2>
+        </div>
       </div>
+      <h2 className='uploadInfo'>You're welcome to share your cherished family recipes here. Our digital repository promises your traditional meals will always be passed on.</h2>
+      <a className='uploadInfo' href='/upload'> Upload Here</a>
     </div>
-    <div className="triangleContainer">
-      <img className='triangle' src={triangleBackground} alt="Triangle Background" />
-      <div className="overlay">
-        <h2>DECADES OF DELICIOUSNESS</h2>
-      </div>
-    </div>
-    <h2 className='uploadInfo'>You're welcome to share your cherished family recipes here. Our digital repository promises your traditional meals will always be passed on.</h2>
-    <a className='uploadInfo' href='/upload'> Upload Here</a>
-  </div>
   );
 }
 
